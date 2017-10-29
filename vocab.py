f057b21b6ca7855f1c1056416390d8c674c8b2f2
a.zolotarev <magic@front.ru>
2017-10-29 11:22:09 +0400

    Replace low freq chars with empty chars.

--- vocab.py
+++ vocab.py
@@ -1,4 +1,8 @@
+import math
+
 class Vocab:
+    EMPTY_CHAR = ' '
+
     def __init__(self, data_reader):
         self._data_reader = data_reader
 
@@ -8,9 +12,15 @@ class Vocab:
         self._part2index = {}
         self._index2part = []
 
+        self._char_freq_threshold = 0
+        self._skipped_chars = []
+
         self._loaded = False
 
     def _feed_char(self, char):
+        if char in self._skipped_chars:
+            return
+
         if char not in self._char2index:
             index = len(self._char2index)
             self._char2index[char] = index
@@ -23,7 +33,7 @@ class Vocab:
             self._index2part.append(speech_part)
 
     def _load_initial_chars(self):
-        self._feed_char(' ')
+        self._feed_char(self.EMPTY_CHAR)
         self._feed_char('{')
         self._feed_char('}')
 
@@ -42,15 +52,31 @@ class Vocab:
         for part in sorted(uniq_speech_parts):
             self._feed_speech_part(part)
 
+    def _calculate_char_freq_threshold(self):
+         _char, max_freq = self._data_reader.get_chars_freq()[-1]
+         self._char_freq_threshold = math.ceil(max_freq * 0.01) # 1% of max freshold
+
+    def _find_low_freq_chars(self):
+        self._calculate_char_freq_threshold()
+
+        for char, freq in self._data_reader.get_chars_freq():
+            if freq <= self._char_freq_threshold:
+                self._skipped_chars.append(char)
+            else:
+                break
+
     def load(self):
+        self._find_low_freq_chars()
+
         self._load_chars()
         self._load_speech_parts()
+
         self._loaded = True
 
     def char_to_index(self, char):
         if not self._loaded:
             raise BaseException('chars not loaded')
-        return self._char2index[char]
+        return self._char2index.get(char) or self._char2index[self.EMPTY_CHAR]
 
     def part_to_index(self, speech_part):
         if not self._loaded:
@@ -62,3 +88,18 @@ class Vocab:
 
     def part_vocab_size(self):
         return len(self._index2part)
+
+if __name__ == '__main__':
+    # debug
+    from data_reader import DataReader
+    SENTENCES_SOURCE = 'data/sentences.xml'
+
+    data_reader = DataReader(SENTENCES_SOURCE)
+    data_reader.load()
+
+    vocab = Vocab(data_reader)
+    vocab.load()
+
+    print('skipped chars threshold', vocab._char_freq_threshold)
+    print('skipped chars', vocab._skipped_chars)
+    print('vocab', vocab._char2index)
