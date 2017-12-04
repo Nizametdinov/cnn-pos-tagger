b60f9f22703d97cfaeaa69e36fe283d1ef5d2f5d
Azat Nizametdinov <anizametdinov@gmail.com>
2017-12-04 21:24:44 +0400

    Create dir before data downloading

--- download_data.py
+++ download_data.py
@@ -1,6 +1,9 @@
 import bz2
 import urllib.request
+import os
+import os.path
 
+DATA_DIR = 'data'
 OPEN_CORPORA_URL = 'http://opencorpora.org/files/export/annot/annot.opcorpora.no_ambig.xml.bz2'
 OPEN_CORPORA_DEST_FILE = 'data/annot.opcorpora.no_ambig.xml'
 CHUNK = 16 * 1024
@@ -18,4 +21,6 @@ def download_and_unbzip(url, dest_file):
 
 
 if __name__ == '__main__':
+    if not os.path.isdir(DATA_DIR):
+        os.mkdir(DATA_DIR)
     download_and_unbzip(OPEN_CORPORA_URL, OPEN_CORPORA_DEST_FILE)
